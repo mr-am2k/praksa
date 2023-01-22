6d5d6fcd99f8bf77447b113bd9965dfe611b065a
Muamer Aličković <alickovicmuamer1@gmail.com>
2023-01-22 11:23:04 +0100

    Finished login and register

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,9 +3,25 @@ const {StatusCodes} = require('http-status-codes')
 
 const registerUser = async (req, res) => {
     const sentUser = req.body
+    const exists = await User.find({"username": sentUser.username})
+
+    if(exists.length) {
+        return res.status(StatusCodes.BAD_REQUEST).json({message: "Korisnik sa tim username-om vec postoji"})
+    }
     const user = await User.create(sentUser);
     const {...returnObject} = user._doc;
     res.status(StatusCodes.CREATED).json({user:returnObject})
 }
 
-module.exports = {registerUser}
+const loginUser = async (req, res) => {
+    const sentUser = req.body
+    const exists = await User.find({"username": sentUser.username, "password": sentUser.password})
+    console.log(exists)
+    if(!exists.length){
+        return res.status(StatusCodes.BAD_REQUEST).json({message: "Ne postoji korisnik sa tim podacima"})
+    }
+
+    res.status(StatusCodes.OK).json({user:exists})
+}
+
+module.exports = {registerUser, loginUser}
--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
     fullName:{type:String},
     username:{type:String},
     password:{type:String},
-    typeOfUse:{type:String}
+    typeOfUser:{type:String}
 })
 
 module.exports = mongoose.model('User', UserSchema)
