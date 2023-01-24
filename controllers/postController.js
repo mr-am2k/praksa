99211718e09414a1187b58788a94b98c9a68a734
Muamer Aličković <alickovicmuamer1@gmail.com>
2023-01-24 18:22:17 +0100

    Added logic for returning posts for specific user, logic for deleting post

--- controllers/postController.js
+++ controllers/postController.js
@@ -30,6 +30,7 @@ const getPosts = async (req, res) => {
   const { location, company, technologies } = req.body;
 
   let filters = {};
+  const now = new Date();
 
   if (location) {
     filters.location = { $regex: new RegExp(location, 'i') };
@@ -44,6 +45,8 @@ const getPosts = async (req, res) => {
       $in: technologies.map(tech => new RegExp(tech, 'i')),
     };
   }
+
+  filters.endDate = { $gt: now };
 
   const foundPosts = await Post.find(filters);
 
@@ -82,7 +85,8 @@ const getPosts = async (req, res) => {
 
 const getPost = async (req, res) => {
   const postId = req.params.postId;
-  const foundPost = await Post.findById(postId);
+  const now = new Date();
+  const foundPost = await Post.find({ id: postId, endDate: { $gt: now } });
   const { ...returnObject } = foundPost._doc;
   delete returnObject.__v;
 
@@ -112,4 +116,50 @@ const getPost = async (req, res) => {
   res.status(StatusCodes.OK).json(returnObject);
 };
 
-module.exports = { addPost, getPosts, getPost };
+const getMyPosts = async (req, res) => {
+  const userId = req.params.userId;
+  const now = new Date();
+  const foundedPosts = await Post.find({ user: userId, endDate: { $gt: now } });
+
+  const returnPosts = foundedPosts.map(post => {
+    const { __v, ...restOfTheObject } = post;
+    const postsWithoutV = restOfTheObject._doc;
+    delete postsWithoutV.__v;
+
+    const endDate = new Date(postsWithoutV.endDate);
+    const currentDate = new Date();
+
+    var diffInDays = (endDate - currentDate) / (1000 * 3600 * 24);
+
+    if (diffInDays < 30) {
+      const returnStringDays =
+        diffInDays == 1 ? `${diffInDays} dan` : `${diffInDays} dana`;
+      postsWithoutV.endDate = returnStringDays;
+    } else {
+      const months = diffInDays / 30;
+      const days = diffInDays % 30;
+      const returnStringMonths =
+        months.toFixed(0) == 1
+          ? `${months.toFixed(0)} mjesec`
+          : `${months.toFixed(0)} mjeseci`;
+      const returnStringDays =
+        days.toFixed(0) == 1
+          ? `${days.toFixed(0)} dan`
+          : `${days.toFixed(0)} dana`;
+      postsWithoutV.endDate = `${returnStringMonths} ${returnStringDays}`;
+    }
+    return postsWithoutV;
+  });
+
+  res.status(StatusCodes.OK).json(returnPosts);
+};
+
+const deletePost = async (req, res) => {
+  const postId = req.params.postId;
+
+  await Post.findByIdAndDelete({_id: postId});
+
+  res.status(StatusCodes.OK).json(true)
+};
+
+module.exports = { addPost, getPosts, getPost, getMyPosts, deletePost };
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const {addPost, getPost, getPosts} = require('../controllers/postController')
+const {addPost, getPost, getPosts,getMyPosts, deletePost} = require('../controllers/postController')
 
 router.post('/', addPost)
 router.get('/', getPosts)
 router.get('/:postId', getPost)
+router.get('/my-posts/:userId', getMyPosts)
+router.delete('/:postId', deletePost)
+
 
 module.exports = router
